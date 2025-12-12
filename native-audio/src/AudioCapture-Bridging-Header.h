e8abad436c4757e20584cbbe97c884b2332c7081
Nakul Gupta <nakul@growhut.in>
2025-12-12 14:10:39 +0530

    removed .swift

--- native-audio/src/AudioCapture-Bridging-Header.h
+++
@@ -1,5 +0,0 @@
-//
-//  Bridging header for Swift/Objective-C interop
-//  Swift will import frameworks directly, this is minimal
-//
-
--- native-audio/src/AudioCapture.swift
+++
@@ -1,3 +0,0 @@
-// This file is no longer needed - using pure Objective-C++ implementation
-// Keeping for reference
-
